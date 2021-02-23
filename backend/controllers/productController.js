c0d834bf1ed658243628c3161ac228416eb93db9
Thành Quý <thanhquy1105@gmail.com>
2021-02-23 22:16:51 +0700

    Update Filter, Pagination, Search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,6 +4,7 @@ const ErrorHandler = require("../utils/errorHandler");
 
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
+const APIFeatures = require("../utils/apiFeatures");
 //Create new product => api/v1/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -13,12 +14,22 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+//Get all products => /api/v1/product?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const resPerPage = 4;
+
+  const productCount = await Product.countDocuments();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resPerPage);
+
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
     count: products.length,
+    productCount,
     products,
   });
 });
