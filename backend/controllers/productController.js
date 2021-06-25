06936fb441ebf384187949b7c5ea37ed5e999074
Thành Quý <thanhquy1105@gmail.com>
2021-06-25 20:49:36 +0700

    Implementing Redux for product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,9 +17,10 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get all products => /api/v1/product?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const resPerPage = 4;
 
-  const productCount = await Product.countDocuments();
+  const resPerPage = 8;
+
+  const productsCount = await Product.countDocuments();
   const apiFeatures = new APIFeatures(Product.find(), req.query)
     .search()
     .filter()
@@ -29,12 +30,12 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
-    count: products.length,
-    productCount,
+    productsCount,
     products,
   });
 });
 
+// Get single product details => /api/v1/product/:id
 exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
