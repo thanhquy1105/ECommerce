6744fd96c2151a8a7fb642149f3f5c82f4c45d5f
Thành Quý <thanhquy1105@gmail.com>
2021-07-05 03:21:26 +0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ process.on("uncaughtException", (err) => {
 
 //Setting up config file
 if (process.env.NODE_ENV !== "PRODUCTION")
-  require("dotenv").dotenv.config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
